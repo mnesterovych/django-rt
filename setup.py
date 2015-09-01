10452fb36feb8ad22c6bd3983e240851a23e2e75
Mark Nesterovych <m.nesterovych@gmail.com>
2015-09-01 12:38:44 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-rt',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/mnesterovych/django-rt.git',
+    license='',
+    author='Mark Nesterovych',
+    author_email='m.nesterovych@gmail.com',
+    description='django backend to work with RequestTracker'
+)
